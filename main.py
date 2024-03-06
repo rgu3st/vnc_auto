557bfd928397fbca852af480cbc709e187c9d8f8
Rob <robguest@gmail.com>
2024-03-06 18:11:11 -0500

    Basic client/server set up seems to work now. Needs cleanup.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import subprocess
+import time
 import argparse
 import socket
 import netifaces
 
-
+BROADCAST_INTERVAL =  6  # In seconds
+SUBNET = '192.168.0'
 
 def arg_init():
 	parser = argparse.ArgumentParser()
@@ -16,47 +18,56 @@ def arg_init():
 class comms:
 	def __init__(self, s_c_type):
 		# ip_udp_identifier = b"*automagic*"
-		UDP_PORT = 41414
-		UDP_IP = "127.0.0.1"
-		if s_c_type == "s":  # Hacky...
-			UDP_IP = "192.168.0.3"
+		self.UDP_PORT = 41414
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		# self.sock.bind((UDP_IP, UDP_PORT))
 
 
-	def send_server_ip(self):
+	def send_server_info(self, info:str):
 		''' Server-side to broadcast its IP. '''
-		# TODO: set this up to have a reference to THIS server if 
-		message = self.server.ip_address
+		print(f"Broadcasting my  server info on {SUBNET}.255:{self.UDP_PORT}...")
 		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-		self.sock.sendto(message, ('255.255.255.255', UPD_PORT)
-				
+		broadcast_ip = SUBNET + ".255"
+		self.sock.sendto(info.encode('utf8'), (broadcast_ip, self.UDP_PORT))  				
 
-	def listen_for_ip(self):
+
+	def client_listen_for_info(self):
 		'''Client-side'''
-		self.sock.bind(('192.168.0.255', UDP_PORT))  # The 'broadcast' ip should be sub.net.addr.255.
-		data, addr = sock.recvfrom(1024)  # I'm pretty sure this is blocking: which I want here...
-		print("Connecting to server at ip: {}")	
+		print(f"Waiting for a vnc server on {SUBNET}.255:{self.UDP_PORT}...")
+		self.sock.bind(('192.168.0.255', self.UDP_PORT))  # The 'broadcast' ip should be sub.net.addr.255.
+		data, addr = self.sock.recvfrom(1024)  # I'm pretty sure this is blocking: which I want here...
+		print(f"Got server info: {data.decode('utf8')} from {addr}")	
+		return data.decode('utf8')
 
 
 class client:
 	def __init__(self):
-		self.server_ips = []	
+		self.server_infos = []	
+
 
 	serverip = "192.168.0.3"
 	port = 5900
 	vnc_viewer_command = f"vncviewer {serverip}::{port}"
 
-	def start():
+	def start(self):
+
+		com = comms("c")  # Too hackey: TODO: something better!
+		server_info = com.client_listen_for_info()
+		if server_info is None:
+			print("Error receiving server info.")
+			return None
+		self.server_infos.append(server_info)
 		subprocess.run(client.vnc_viewer_command.split())
 
 class server:
 	def __init__(self):
 		self.ip_address = self.get_server_ip()
+		self.is_running = False
+		self.com = comms('s')  # TODO: decide if this can be static
 
 	vnc_start_command = "x11vnc -display :0 -ncache 10 -noxdamage -viewonly -shared -many -bg"
 	vnc_stop_command = "killall x11vnc"
 	
+
 	def get_server_ip(self, ip_match="192.168."):
 		ip_list = []
 		ip = "NotFound"  # Try to make it stoopud obvious if not found. 
@@ -65,36 +76,42 @@ class server:
 				for link in netifaces.ifaddresses(interface)[netifaces.AF_INET]:  
 					if ip_match in link['addr']:  # If the link matches, awesome!
 						ip = link['addr']
-						print(f"Found my ip I care about: {ip}!")	
+						# print(f"Found my ip I care about: {ip}!")	
 		return ip
 
-	def stop():
+
+	def broadcast_server_info(self):
+		info = self.ip_address
+		info += " 5900"  # TODO: use the current VNC port, it MAY be 5901, 5902, etc. if multiple x11vnc runs.
+		self.com.send_server_info(info)	
+		print(f"Sent my server info: {info}")  # Should I add a log-level?
+
+
+	def stop(self):
 		subprocess.run(server.vnc_stop_command.split())
 	
-	def start():
-		subprocess.run(server.vnc_start_command.split())
+
+	def start(self):
+		is_running = True
+		subprocess.run(server.vnc_start_command.split())  # TODO: uncomment when ready to actually run VNC!
+		while is_running:
+			self.broadcast_server_info()
+			time.sleep(BROADCAST_INTERVAL)
 
 
 def main(args):
-	com = comms("s")
-	com.send_server_ip()
-
-	serv = server()
-	# serv.get_server_ip()
-
 	if (args.s and args.c) or not (args.s or args.c):
 		print("You have to select either -s or -c!")
 		return
 	if args.s:
-		print(f'Starting sever stuffs with command:\n {server.vnc_start_command}')
-		server.start()
-		quit_input = input("Type 'q' then Return/Enter to stop.\n")
+		serv = server()
+		serv.start()
+		quit_input = input("\nType 'q' then Return/Enter to stop.\n")
 		if quit_input == 'q':
 			server.stop()
 	elif args.c:
-		print(f"Starting to view server {client.serverip} on port {client.port}.")
-		com.get_server_ip()
-		client.start()	
+		clie = client()
+		clie.start()	
 
 
 if __name__ == "__main__":
